1cfe4de80ee0493aae66cb4e10360cf356cbadd6
ScavBob <47184928+ScavBob@users.noreply.github.com>
2020-12-16 17:06:26 +0300

    Create MainMenu.java

---
+++ CS319H_RISK/src/main/java/Screens/MainMenu.java
@@ -0,0 +1,27 @@
+package Screens;
+
+import game.Game;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+public class MainMenu extends Menu {
+
+	   @Override
+	    public Scene getScene() {
+	        Group root = new Group();
+	        Scene scene = new Scene(root);
+	        addButtons(root);
+
+	        return scene;
+	    }
+
+	private void addButtons(Group root) {
+		addTransitionButton(root, "", 100, 100, 75, 439, "img.png", null);
+		addTransitionButton(root, "Load Game", 300, 300, 160, 40,"img.png", null);
+		addTransitionButton(root, "Learn to Conquer", 300, 400, 160, 40,"img.png", null);
+		addTransitionButton(root, "Settings", 300, 450, 160, 40,"img.png", null);
+		addTransitionButton(root, "Exit", 300, 500, 160, 40,"img.png", null);
+	}
+}
