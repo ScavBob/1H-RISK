9098e949d9bea7f372b19321d4156b07b90dad53
scavbob <cankatkadim@gmail.com>
2020-12-16 19:54:59 +0300

    add Game.java and GameLauncher.java

--- CS319H_RISK/src/main/java/game/Game.java
+++ CS319H_RISK/src/main/java/game/Game.java
@@ -1,15 +1,20 @@
 package game;
 
+import screens.MainMenu;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import managers.GameManager;
 
-public class Game {
+public class Game{
 
     private static Game gameInstance = null;
-
-    private GameManager gameManager;
+    private static Stage gameStage = null;
+    private GameManager gameManager = null;
 
     private Game()
     {
+        gameStage = new Stage();
 
     }
 
@@ -23,5 +28,17 @@ public class Game {
     public GameManager getGameManager()
     {
         return gameManager;
+    }
+
+    public Stage getStage() {
+        return gameStage;
+    }
+
+    public void gameInit(Stage primaryStage){
+        gameStage = primaryStage;
+        gameStage.setScene(new MainMenu().getScene());
+        gameStage.sizeToScene();
+        gameStage.setTitle("Deneme");
+        gameStage.show();
     }
 }
