b885160e076d410be0d5e2b4631f24d3d6543cd5
KAAN ATAKAN ARAY <atakanaray@gmail.com>
2020-12-17 11:55:15 +0300

    Bug fixes

--- CS319H_RISK/src/main/java/screens/MainMenu.java
+++ CS319H_RISK/src/main/java/screens/MainMenu.java
@@ -28,9 +28,9 @@ public class MainMenu extends Menu {
 
 	private void addButtons(Group root) {
 		addTransitionButton(root, "", 495, 335, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\NewGame.png", new PregameMenu());
-		addTransitionButton(root, "", 495, 392, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\LoadGame.png", new HowToPlayScreen());
+		addTransitionButton(root, "", 495, 392, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\LoadGame.png",null);
 		addTransitionButton(root, "", 495, 447, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\Options.png", new OptionsMenu());
-		addTransitionButton(root, "", 495, 504, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\HowToPlay.png", null);
+		addTransitionButton(root, "", 495, 504, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\HowToPlay.png",  new HowToPlayScreen());
 		//TODO
 		addButtons(root, "", 495, 561, 294, 51, StorageManager.RESOURCES_FOLDER_NAME + "MainMenu\\Exit.png", new EventHandler<ActionEvent>() {
 			@Override
