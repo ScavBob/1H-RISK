00f1c412431fe467b601fa78a7fd62dcda01f349
Ruzgar <zzlawlzz52@gmail.com>
2020-12-18 00:51:12 +0300

    Changed MapManager.

--- CS319H_RISK/src/main/java/managers/MapManager.java
+++ CS319H_RISK/src/main/java/managers/MapManager.java
@@ -8,8 +8,18 @@ import java.nio.file.Paths;
 
 public class MapManager
 {
-    public Map getMap(){
-        URL url = getClass().getResource("/Game/worldmapdata.txt");
+    public static final int WORLD_MAP = 1;
+    public static final int BILKENT_MAP = 2;
+
+    public Map getMap(int map){
+        URL url = null;
+        if (map == WORLD_MAP)
+            url = getClass().getResource("/Game/worldmapdata.txt");
+        else if (map == BILKENT_MAP)
+            url = getClass().getResource("/Game/bilkentmapdata.txt");
+
+        if (url == null)
+            return null;
         File file = new File(url.getPath());
         return new Map(file);
     }
