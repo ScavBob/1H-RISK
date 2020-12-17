a14f660ac377edc791cf7d43ffcd8c310779f9e2
Ruzgar <zzlawlzz52@gmail.com>
2020-12-18 01:39:28 +0300

    Adding random map and army generation.

--- CS319H_RISK/src/main/java/managers/GameManager.java
+++ CS319H_RISK/src/main/java/managers/GameManager.java
@@ -30,6 +30,7 @@ public class GameManager
             match.addPlayer(playerToAdd);
         Map matchMap = Game.getInstance().getGameManager().mapManager.getMap(MapManager.WORLD_MAP);
         match.setMap(matchMap);
+        match.initialize();
     }
 
     public Match getMatch() {
