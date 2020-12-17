8fc006297a5bc0c0cc96d34a4e4c714d16aa0913
kaanerkan <71774614+kaanerkan@users.noreply.github.com>
2020-12-17 16:46:22 +0300

    Update Match.java
    match class implementation

--- CS319H_RISK/src/main/java/application/Match.java
+++ CS319H_RISK/src/main/java/application/Match.java
@@ -7,10 +7,35 @@ public class Match{
     private int round;
     private int maxRound;
     private ArrayList<Player> players;
+    private Player currentPlayer;
     private ArrayList<Mission> missionList;
     private HashMap <Faction,Boolean> availableFactions;
     private Map map;
     private GameController controller;
 
-    public void nextTurn(){}
+    //constructor
+    public Match(int round, int maxRound, ArrayList<Player> players,
+                 ArrayList<Mission> missionList,HashMap <Faction,Boolean> availableFactions,
+                 Map map, GameController controller){
+        this.round = 0;
+        this.maxRound = maxRound;
+        this.players = players;
+        this.missionList = missionList;
+        this.currentPlayer = players.get(0);
+        this.availableFactions = availableFactions;
+        this.map = map;
+        this.controller = controller;
+    }
+    //skips to the next player in line
+    public void nextTurn(){
+        try {
+            if(round < maxRound) {
+                round++;
+                currentPlayer = players.get(round % players.size());
+            }
+        } catch(Exception e) {
+            System.out.println("Maximum round number has been reached");
+        }
+
+    }
 }
