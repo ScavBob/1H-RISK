279bc33e94b7a8aa56092d23ee35b17aa4fc7d55
scavbob <cankatkadim@gmail.com>
2020-12-20 13:35:24 +0300

    GameScreen resource addition method changed

--- CS319H_RISK/src/main/java/screens/GameScreen.java
+++ CS319H_RISK/src/main/java/screens/GameScreen.java
@@ -22,9 +22,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import managers.StorageManager;
 
-import java.nio.file.Paths;
 import java.util.List;
 
 public class GameScreen implements UpdatableScreen{
@@ -53,9 +51,9 @@ public class GameScreen implements UpdatableScreen{
         root.getChildren().clear();
         canvas = new Canvas(1280, 720);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.drawImage(new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\" + Game.getInstance().getGameManager().getMatch().getMap().getMapName() + "Map.png").toUri().toString()), 0, 0, 1280, 720);
+        gc.drawImage(new Image(getClass().getResource("/GameResources/" + Game.getInstance().getGameManager().getMatch().getMap().getMapName() + "Map.png").toExternalForm()), 0, 0, 1280, 720);
         root.getChildren().add(canvas);
-        gc.drawImage(new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\Phases\\" + Game.getInstance().getGameManager().getMatch().getCurrentPhase() + ".png").toUri().toString()), 495, 1, 291, 60);
+        gc.drawImage(new Image(getClass().getResource("/GameResources/Phases/" + Game.getInstance().getGameManager().getMatch().getCurrentPhase() + ".png").toExternalForm()), 495, 1, 291, 60);
         gc.setFill(Paint.valueOf("#0000007f"));
         gc.fillRect(1170, 20, 90, 40);
         populateScreen();
@@ -95,9 +93,9 @@ public class GameScreen implements UpdatableScreen{
         Scene scene = new Scene(root, 500, 270, Color.BLACK);
         Canvas canvas = new Canvas(500, 270);
         dialog.setScene(scene);
-        Image background = new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\RollingDice\\Background.png").toUri().toString());
+        Image background = new Image(getClass().getResource("/GameResources/RollingDice/Background.png").toExternalForm());
         canvas.getGraphicsContext2D().drawImage(background, 0, 0);
-        Image dice = new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\RollingDice\\qm.png").toUri().toString());
+        Image dice = new Image(getClass().getResource("/GameResources/RollingDice/qm.png").toExternalForm());
         for(int i = 0; i < attacking; i++){
             canvas.getGraphicsContext2D().drawImage(dice, 5, 40 + (240/attacking)*i);
         }
@@ -139,7 +137,7 @@ public class GameScreen implements UpdatableScreen{
     }
 
     public int showBattleResults(List<Integer> attackerDice, List<Integer> defenderDice, List<Boolean> results, boolean won, int maxMovableArmies){
-        currentArmyCount = 0;
+        currentArmyCount = 1;
         Stage dialog = new Stage();
         dialog.setResizable(false);
         dialog.initStyle(StageStyle.UTILITY);
@@ -148,15 +146,15 @@ public class GameScreen implements UpdatableScreen{
         scene.getStylesheets().add("style.css");
         Canvas canvas = new Canvas(500, 270);
         dialog.setScene(scene);
-        Image background = new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\RollingDice\\Background.png").toUri().toString());
+        Image background = new Image(getClass().getResource("/GameResources/RollingDice/Background.png").toExternalForm());
         canvas.getGraphicsContext2D().drawImage(background, 0, 0);
         Timeline timeline = new Timeline();
         for(int i = 0; i < defenderDice.size(); i++){
             int x = 25;
             int y = 40 + (240/defenderDice.size())*i;
-            Image dice = new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\RollingDice\\" + attackerDice.get(i) + ".png").toUri().toString());
+            Image dice = new Image(getClass().getResource("/GameResources/RollingDice/" + attackerDice.get(i) + ".png").toExternalForm());
             canvas.getGraphicsContext2D().drawImage(dice, x, y);
-            dice = new Image(Paths.get(StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\RollingDice\\" + defenderDice.get(i) + ".png").toUri().toString());
+            dice = new Image(getClass().getResource("/GameResources/RollingDice/" + defenderDice.get(i) + ".png").toExternalForm());
             canvas.getGraphicsContext2D().drawImage(dice, x + 385, y);
         }
         root.getChildren().add(canvas);
@@ -207,13 +205,13 @@ public class GameScreen implements UpdatableScreen{
             addLabels(r);
         }
 
-        addButton(root, "", 575, 645, 75, 75, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Pause-Skip\\Pause.png", new EventHandler<ActionEvent>() {
+        addButton(root, "", 575, 645, 75, 75, getClass().getResource("/GameResources/UI/Pause-Skip/Pause.png").toExternalForm(), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Game.getInstance().setScreen(new PauseMenu());
             }
         });
-        addButton(root, "", 630, 645, 75, 75, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Pause-Skip\\Skip.png", new EventHandler<ActionEvent>() {
+        addButton(root, "", 630, 645, 75, 75, getClass().getResource("/GameResources/UI/Pause-Skip/Skip.png").toExternalForm(), new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
                         Game.getInstance().getGameManager().getInputManager().endPhase();
@@ -223,7 +221,7 @@ public class GameScreen implements UpdatableScreen{
     }
 
     private void addElements(int phase){
-        addButton(root, "", 0, 420, 250 , 300, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Source-Destination\\Background.png", null);
+        addButton(root, "", 0, 420, 250 , 300, getClass().getResource("/GameResources/UI/Source-Destination/Background.png").toExternalForm(), null);
         String color = Paint.valueOf(Game.getInstance().getGameManager().getMatch().getCurrentPlayer().getColor()).toString();
         color = color.substring(0, color.length() - 2) + "5f";
         canvas.getGraphicsContext2D().setFill(Paint.valueOf(color));
@@ -275,7 +273,7 @@ public class GameScreen implements UpdatableScreen{
             }
         }
         addText(String.valueOf(armyCount), 105, 610, 25);
-        addButton(root, "", 150, 575, 50, 50, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Source-Destination\\Add.png", new EventHandler<ActionEvent>() {
+        addButton(root, "", 150, 575, 50, 50, getClass().getResource("/GameResources/UI/Source-Destination/Add.png").toExternalForm(), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 if(armyCount + 1 <= Game.getInstance().getGameManager().getInputManager().getMaxChoosableArmy())
@@ -283,7 +281,7 @@ public class GameScreen implements UpdatableScreen{
                 Game.getInstance().updateScreen();
             }
         });
-        addButton(root, "", 25, 575, 50, 50, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Source-Destination\\Remove.png", new EventHandler<ActionEvent>() {
+        addButton(root, "", 25, 575, 50, 50, getClass().getResource("/GameResources/UI/Source-Destination/Remove.png").toExternalForm(), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 if(armyCount - 1 >= 0)
@@ -291,7 +289,7 @@ public class GameScreen implements UpdatableScreen{
                 Game.getInstance().updateScreen();
             }
         });
-        addButton(root,"", 115, 650, 50, 50, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Source-Destination\\Done.png", new EventHandler<ActionEvent>() {
+        addButton(root,"", 115, 650, 50, 50, getClass().getResource("/GameResources/UI/Source-Destination/Done.png").toExternalForm(), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 if(Game.getInstance().getGameManager().getInputManager().getFirstRegion() != null) {
@@ -301,7 +299,7 @@ public class GameScreen implements UpdatableScreen{
                 }
             }
         });
-        addButton(root, "", 55, 650, 50, 50, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\UI\\Source-Destination\\Cancel.png", new EventHandler<ActionEvent>() {
+        addButton(root, "", 55, 650, 50, 50, getClass().getResource("/GameResources/UI/Source-Destination/Cancel.png").toExternalForm(), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                Game.getInstance().getGameManager().getInputManager().resetRegions();
@@ -334,14 +332,14 @@ public class GameScreen implements UpdatableScreen{
                 update();
             }
         };
-        addButton(root, unitCount, x - 10, y - 10, 25, 25, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\Region\\" + region.getController().getColor() + "Label.png", handler).setTextFill(Paint.valueOf("#ffffffff"));
+        addButton(root, unitCount, x - 10, y - 10, 25, 25, getClass().getResource("/GameResources/Region/" + region.getController().getColor() + "Label.png").toExternalForm(), handler).setTextFill(Paint.valueOf("#ffffffff"));
 
-        addButton(root, regionName, x+regionName.length()/2-25, y + 15, regionName.length() + 20, 25, StorageManager.RESOURCES_FOLDER_NAME + "GameResources\\Region\\Namebar.png", handler).setTextFill(Paint.valueOf("#ffffffff"));
+        addButton(root, regionName, x+regionName.length()/2-25, y + 15, regionName.length() + 20, 25, getClass().getResource("/GameResources/Region/Namebar.png").toExternalForm(), handler).setTextFill(Paint.valueOf("#ffffffff"));
     }
 
     private Button addButton(Group root, String title, int x, int y, int width, int height, String imagePath, EventHandler<ActionEvent> event){
         Button button = new Button(title);
-        Image image = new Image(Paths.get(imagePath).toUri().toString());
+        Image image = new Image(imagePath);
         if(image != null)
             button.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
         button.setLayoutX(x);
