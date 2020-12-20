5917f271c01e94c58f36cb9abdf2879a7bd029ca
Ruzgar <zzlawlzz52@gmail.com>
2020-12-20 16:13:12 +0300

    deleting old unused methods

--- CS319H_RISK/src/main/java/application/BattleManager.java
+++ CS319H_RISK/src/main/java/application/BattleManager.java
@@ -13,28 +13,6 @@ import java.util.List;
 import javax.lang.model.util.ElementScanner6;
 
 public class BattleManager implements Serializable {
-    private ArrayList<Player> players;
-
-    /**
-     *    Return 1 means attacker wins the battle,
-     *    Return 0 means they are even,
-     *    Return -1 means defender wins the battle.
-     *
-     * @return the coded result of the battle.
-     */
-    public int battle(Player attacker, int attackerSoldiers, Player defender, int defenderSoldiers, Card location) {
-        int remainingSoldier;
-        if (attackerSoldiers > defenderSoldiers) {
-            remainingSoldier = attackerSoldiers - defenderSoldiers;
-            return 1;
-        } else if (attackerSoldiers == defenderSoldiers) {
-            remainingSoldier = 0;
-            return 0;
-        } else {
-            remainingSoldier = defenderSoldiers - attackerSoldiers;
-            return -1;
-        }
-    }
 
     private int rollDice()
     {
